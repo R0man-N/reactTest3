ffa1ca6c341038fe5f8488e1171823fd0ad6146d
Roman Novosadenko <142111532+R0man-N@users.noreply.github.com>
2024-03-07 09:08:23 +0200

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,23 @@
 // src/components/Product.jsx
 
-export const Product = () => {
-  return (
-    <div>
-      <h2>Tacos</h2>
-      <img
-        src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>"
-        alt="Tacos With Lime"
-        width="640"
-      />
-      <p>Price: 999 credits</p>
-    </div>
-  );
+import PropTypes from "prop-types";
+
+const Product = ({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) => (
+  <div>
+    <img src={imgUrl} alt={name} width="480" />
+    <h2>{name}</h2> <p>Price: {price} credits</p>{" "}
+  </div>
+);
+
+// Опис типів пропсів компонента
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string,
+  price: PropTypes.number.isRequired,
 };
+
+export default Product;
